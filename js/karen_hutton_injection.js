256789d83d6be39934cda22601dd90d050e07bbe
Mohamed Mansour <hello@mohamedmansour.com>
2012-03-03 14:13:50 -0500

    Support +1 on comments

--- js/karen_hutton_injection.js
+++ js/karen_hutton_injection.js
@@ -1,10 +1,10 @@
-var PLUS_ADD_POST_NAME = 'Click to +1 this post';
-var PLUS_REMOVE_POST_NAME = 'Click to remove your +1 from this post';
-var PLUS_ADD_BUTTON_SELECTOR = 'button[title="' + PLUS_ADD_POST_NAME + '"]';
-var PLUS_REMOVE_BUTTON_SELECTOR = 'button[title="' + PLUS_REMOVE_POST_NAME + '"]';
+var PLUS_ADD_POST_NAME = 'Click to +1 this ';
+var PLUS_REMOVE_POST_NAME = 'Click to remove your +1 from this ';
+var PLUS_ADD_BUTTON_SELECTOR = 'button[title^="' + PLUS_ADD_POST_NAME + '"]';
+var PLUS_REMOVE_BUTTON_SELECTOR = 'button[title^="' + PLUS_REMOVE_POST_NAME + '"]';
 
 var plusClicked = function(e) {
-  var isPlussed = e.target.title == PLUS_ADD_POST_NAME;
+  var isPlussed = e.target.title.indexOf(PLUS_ADD_POST_NAME) == 0;
   chrome.extension.sendRequest({method: 'PlaySound', state: isPlussed});
 };
 
